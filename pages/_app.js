265b9da4cd52e040468a8ab0b14fa52102012cb1
kevindavid <kevin.david005006@gmail.com>
2020-09-15 16:10:40 +0200

    Add i18n

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import App from 'next/app'
+import Head from 'next/head'
+
+import { appWithTranslation } from '../i18n'
+
+import '../styles/_globals.sass'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const head = (
+    <Head>
+      <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"></link>
+      <meta name="robots" content="noindex, nofollow"></meta>
+    </Head>
+  )
+
+  return (
+    <div>
+      {head}
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
-export default MyApp
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext)
+  return { ...appProps }
+}
+
+export default appWithTranslation(MyApp)
