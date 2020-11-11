7e6dc75e6a4b0244f7a92cd0fcf0d3dffe8e3d74
kevindavid <kevin.david005006@gmail.com>
2020-11-11 17:29:11 +0100

    PAUSE : triggered when selecting a tool / SET_PREVIEW_VIDEO : triggered when selecting videos tool

--- components/Campaign/Tools.js
+++ components/Campaign/Tools.js
@@ -4,10 +4,12 @@ import styles from '@/styles/components/Campaign/Tools.module.sass'
 
 const Tools = () => {
   const dispatch = useDispatch()
+  const isPlaying = useSelector(state => state.campaign.isPlaying)
   const tool = useSelector(state => state.campaign.tool)
   
   const selectTool = (clickedTool, element) => {
     dispatch({ type: 'SELECT_TOOL', data: tool === clickedTool ? 0 : clickedTool })
+    isPlaying ? dispatch({ type: 'PAUSE' }) : ''
     tool === clickedTool ? dispatch({ type: 'HIDE_PREVIEW' }) : dispatch({ type: 'SHOW_PREVIEW', data: { element } })
   }
 
@@ -22,7 +24,10 @@ const Tools = () => {
       </li>
       <li
         className={`${styles.tool} ${tool === 2 ? styles.toolSelected : ''}`}
-        onClick={() => selectTool(2, 'video')}
+        onClick={() => {
+          dispatch({ type: 'SET_PREVIEW_VIDEO', data: '' })
+          selectTool(2, 'video')
+        }}
       >
         <img src="/assets/campaign/toolVideos.svg" />
         <p>Videos</p>
