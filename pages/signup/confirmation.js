6bdd18441b9c5a3e0ba153d0435e2e422bc78332
kevindavid <kevin.david005006@gmail.com>
2021-03-31 18:19:03 +0200

    Add email confirmation page

--- pages/signup/confirmation.js
+++ pages/signup/confirmation.js
@@ -1,28 +1,50 @@
 import Head from 'next/head'
 
+import { mainAPI } from '@/plugins/axios'
+
+import withAuthServerSideProps from '@/hocs/withAuthServerSideProps'
+
 import SignupLayout from '@/layouts/SignupLayout'
+
+import Button from '@/components/Button'
 
 import styles from '@/styles/layouts/Signup.module.sass'
 
-const SignupConfirmation = ({ user }) => {
+const SignupConfirmation = ({ error }) => {
   return (
     <SignupLayout>
       <Head>
         <title>Sign up | FOMO</title>
       </Head>
 
-      <h1 className={styles.title}>Last step !</h1>
-      <h2 className={styles.subtitle}>Enter these details to complete your account.</h2>
+      <h1 className={styles.title}>Email confirmation</h1>
 
+      { error &&
+        <div className={styles.emailConfirmationError}>
+          <p>Your confirmation link is expired or incorrect.</p>
+          <Button
+            href="/"
+            type="link"
+          >
+            Back to home
+          </Button>
+        </div>
+      }
     </SignupLayout>
   )
 }
 
 export default SignupConfirmation
-// export const getServerSideProps = withAuthServerSideProps((ctx, user) => {
-//   if (user && user.firstName && user.lastName) {
-//     ctx.res.writeHead(302, { Location: '/app' });
-//     ctx.res.end();
-//   }
-//   return false
-// });
+export const getServerSideProps = withAuthServerSideProps(async (ctx, user) => {
+  if (!ctx.query.t) {
+    return { error: true}
+  }
+  try {
+    await mainAPI.post(`/auth/email/confirmation/${ctx.query.t}`)
+    ctx.res.writeHead(302, { Location: '/app' })
+    ctx.res.end()
+    return {}
+  } catch (err) {
+    return { error: true }
+  }
+});
