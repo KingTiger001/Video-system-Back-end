f28c8c6d20d0c6858a3e90c77b484bb400629d0b
kevindavid <kevin.david005006@gmail.com>
2021-04-02 09:51:25 +0200

    Fix email confirmation

--- pages/signup/confirmation.js
+++ pages/signup/confirmation.js
@@ -1,8 +1,10 @@
+import jscookie from 'js-cookie'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 import { mainAPI } from '@/plugins/axios'
-
-import withAuthServerSideProps from '@/hocs/withAuthServerSideProps'
+import dayjs from '@/plugins/dayjs'
 
 import SignupLayout from '@/layouts/SignupLayout'
 
@@ -10,7 +12,27 @@ import Button from '@/components/Button'
 
 import styles from '@/styles/layouts/Signup.module.sass'
 
-const SignupConfirmation = ({ error }) => {
+const SignupConfirmation = () => {
+  const router = useRouter()
+
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    async function confirmEmail () {
+      if (router.query.t) {
+        try {
+          const { data: { jwt, user } } = await mainAPI.post(`/auth/email/confirmation/${router.query.t}`)
+          jscookie.set('fo_sas_tk', jwt, { expires: dayjs().add(30, 'day').toDate() })
+          mainAPI.defaults.headers.common.Authorization = `Bearer ${jwt}`
+          router.replace('/app')
+        } catch (err) {
+          return setError(true)
+        }
+      }
+    }
+    confirmEmail()
+  }, [router])
+
   return (
     <SignupLayout>
       <Head>
@@ -30,21 +52,29 @@ const SignupConfirmation = ({ error }) => {
           </Button>
         </div>
       }
+      { !error && <p className={styles.emailConfirmationChecking}>Checking...</p>}
     </SignupLayout>
   )
 }
 
 export default SignupConfirmation
-export const getServerSideProps = withAuthServerSideProps(async (ctx, user) => {
-  if (!ctx.query.t) {
-    return { error: true}
-  }
-  try {
-    await mainAPI.post(`/auth/email/confirmation/${ctx.query.t}`)
-    ctx.res.writeHead(302, { Location: '/app' })
-    ctx.res.end()
-    return {}
-  } catch (err) {
-    return { error: true }
-  }
-});
+// export const getServerSideProps = async ({ query, req, res }) => {
+//   const cookies = new Cookies(req, res)
+//   if (!query.t) {
+//     return { props: { error: true }}
+//   }
+//   try {
+//     const { data: { jwt, user } } = await mainAPI.post(`/auth/email/confirmation/${query.t}`)
+//     cookies.set('fo_sas_tk', jwt, { expires: dayjs().add(30, 'day').toDate() })
+//     console.log(mainAPI.defaults, '\n')
+//     console.log(mainAPI.defaults.headers, '\n')
+//     console.log(mainAPI.defaults.headers.common)
+//     mainAPI.defaults.headers.common.Authorization = `Bearer ${jwt}`
+//     res.writeHead(302, { Location: '/app' })
+//     res.end()
+//     return { props: {} }
+//   } catch (err) {
+//     console.log(err)
+//     return { props: { error: true }}
+//   }
+// };
