a2db384823b42531a926d3fc7c8f75058dfe9bfe
kevindavid <kevin.david005006@gmail.com>
2021-04-29 15:32:54 +0000

    add missing imports

--- components/Campaign/Tools/ToolRecord.js
+++ components/Campaign/Tools/ToolRecord.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import Button from '@/components/Button'
